4fc4a3cc9518a5a5f7c1ab20b720e6cfc47eb723
Apple <apple@matts-pro.home>
2023-10-17 16:38:14 +0100

    fixed error in code which meant it would only find the first occurrence of any words

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,16 @@
 def substrings (string, dictionary)
   result = {}
+  string = string.downcase.split
   dictionary.each do |element|
-    string = string.downcase
-    if string.include?(element)
+    string.each do |word|
+    if word.include?(element)
       if result.include?(element)
-        result[element] +=1
+        result[element] += 1
       else
         result[element] = 1
       end
     end
   end
+end
   return result
 end
